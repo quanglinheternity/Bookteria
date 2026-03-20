31067bec17f4861db291cf38aa6f629c0a3c33ee
Quang Linh <nguyenquanglinh0509@gmail.com>
2026-03-20 16:50:35 +0700

    Done tạo page và update lại fontend để load page.

--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.devteria.post.service;
 
+import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.entity.Post;
@@ -8,6 +9,10 @@ import com.devteria.post.repository.PostRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -33,12 +38,22 @@ public class PostService {
         return postMapper.toPostResponse( postRepository.save(post));
 
     }
-    public List<PostResponse> getMyPost(){
+    public PageResponse<PostResponse> getMyPost(int page, int size){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = authentication.getName();
-        return postRepository.findAllByUserId(userId)
-                .stream()
-                .map(postMapper::toPostResponse).toList();
+        Sort sort = Sort.by("createdDate").descending();
+        Pageable pageable = PageRequest.of(page - 1 ,size, sort);
+        var pageData = postRepository.findAllByUserId(userId, pageable);
+//        return postRepository.findAllByUserId(userId)
+//                .stream()
+//                .map(postMapper::toPostResponse).toList();
+        return PageResponse.<PostResponse>builder()
+                .currentPage(page)
+                .pageSize(pageData.getSize())
+                .totalPages(pageData.getTotalPages())
+                .totalElements(pageData.getTotalElements())
+                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .build();
 
     }
 }
