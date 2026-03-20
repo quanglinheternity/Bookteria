b88ecd313b098e83e22912951ffcb453b0a14160
Quang Linh <nguyenquanglinh0509@gmail.com>
2026-03-20 14:00:26 +0700

    Done config với kafka.

--- identity-service/src/main/java/com/devteria/identity/service/UserService.java
+++ identity-service/src/main/java/com/devteria/identity/service/UserService.java
@@ -3,6 +3,7 @@ package com.devteria.identity.service;
 import java.util.HashSet;
 import java.util.List;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -41,6 +42,7 @@ public class UserService {
     ProfileMapper profileMapper;
     PasswordEncoder passwordEncoder;
     ProfileClient profileClient;
+    KafkaTemplate<String, String> kafkaTemplate;
 
     public UserResponse createUser(UserCreationRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
@@ -57,6 +59,9 @@ public class UserService {
         profileRequest.setUserId(user.getId());
 
         profileClient.createProfile(profileRequest);
+
+        //public message to kafka
+        kafkaTemplate.send("onboard-successful","Welcome our new member" + user.getUsername());
         return userMapper.toUserResponse(user);
     }
 
--- notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
+++ notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
@@ -7,10 +7,13 @@ import com.devteria.notification.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
@@ -24,5 +27,9 @@ public class EmailController {
                 .message("Send Mail thành công.")
                 .result(emailService.sendEmail(request))
                 .build();
+    }
+    @KafkaListener(topics = "onboard-successful")
+    public  void  listen(String message){
+        log.info("Message received: {}", message);
     }
 }
