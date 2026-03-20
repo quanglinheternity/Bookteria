ee5416888414d6e3ee0e36e7ca18d06c30ea573b
Quang Linh <nguyenquanglinh0509@gmail.com>
2026-03-20 16:50:35 +0700

    Done tạo page và update lại fontend để load page.

--- post-service/src/main/java/com/devteria/post/controller/PostController.java
+++ post-service/src/main/java/com/devteria/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.devteria.post.controller;
 
 import com.devteria.post.dto.ApiResponse;
+import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.service.PostService;
@@ -8,10 +9,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +25,11 @@ public class PostController {
                 .result(postService.createPost(request))
                 .build();
     }
-    @GetMapping("/my-post")
-    ApiResponse<List<PostResponse>> myPost(){
-        return ApiResponse.<List<PostResponse>>builder()
-                .result(postService.getMyPost())
+    @GetMapping("/my-posts")
+    ApiResponse<PageResponse<PostResponse>> myPost(@RequestParam(value = "page",required = false,defaultValue = "1") int page,
+                                                   @RequestParam(value = "size",required = false,defaultValue = "10") int size){
+        return ApiResponse.<PageResponse<PostResponse>>builder()
+                .result(postService.getMyPost(page, size))
                 .build();
     }
 
--- web-app/src/configurations/configuration.js
+++ web-app/src/configurations/configuration.js
@@ -5,4 +5,6 @@ export const CONFIG = {
 export const API = {
   LOGIN: "/identity/auth/token",
   MY_INFO: "/identity/users/my-info",
+  MY_POST: "/post/my-posts",
+
 };
